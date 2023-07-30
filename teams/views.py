b3c6460554c7d7e0aea245725a0e564c26d35a74
unknown <danieleccb25@hotmail.com>
2023-07-30 16:23:48 -0300

    feat: task 3 ok

--- teams/views.py
+++ teams/views.py
@@ -1,5 +1,23 @@
+from django.forms import model_to_dict
 from rest_framework.views import status, Request, Response, APIView
+from exceptions import *
+
+from utils import data_processing
+from .models import Team
+
 
 class TeamView(APIView):
     def post(self, req: Request) -> Response:
-        return Response
+        try:
+            data_processing(req.data)
+        except (NegativeTitlesError, InvalidYearCupError, ImpossibleTitlesError) as e:
+            return Response({"error": str(e)}, status.HTTP_400_BAD_REQUEST)
+
+        create_team = Team.objects.create(**req.data)
+        converted_team = model_to_dict(create_team)
+        return Response(converted_team, status.HTTP_201_CREATED)
+
+    def get(self, req: Request) -> Response:
+        teams = Team.objects.all()
+        converted_team = [model_to_dict(team) for team in teams]
+        return Response(converted_team, status.HTTP_200_OK)
